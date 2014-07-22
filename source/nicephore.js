1d1b325aaf743602618ff87199a0cefd978413e0
&y <andes.collab@gmail.com>
2014-07-22 00:58:34 -0400

    test improvement + paste

--- source/nicephore.js
+++ source/nicephore.js
@@ -1,11 +1,15 @@
-hilary.use([window], function(ctx, window) {
-	var maps = hilary.resolve('nicephore::observer::maps').init(),
-		utils = hilary.resolve('nicephore::observer::utils').init(maps),
-		models = hilary.resolve('nicephore::observer::models').init(maps),
-		helpers = hilary.resolve('nicephore::observer::helpers').init(maps, utils, models),
-		observer = hilary.resolve('nicephore::observer').init(maps, utils, models, helpers, console);
+hilary.use([window, window.FileReader, window.console, window.JSON], function(ctx, window, FileReader, console, JSON) {
+	"use strict";
+    
+    var makeNicophore = function () {
+        var maps = hilary.resolve('nicephore::observer::maps').init(),
+            utils = hilary.resolve('nicephore::observer::utils').init(maps),
+            models = hilary.resolve('nicephore::observer::models').init(console, FileReader),
+            helpers = hilary.resolve('nicephore::observer::helpers').init(maps, utils, models),
+            observer = hilary.resolve('nicephore::observer').init(maps, utils, models, helpers, console, JSON);
+        
+        return observer;
+    };
 
-		observer.start();
-
-		window.nicephore = observer;
+    window.nicephore = makeNicophore;
 });
